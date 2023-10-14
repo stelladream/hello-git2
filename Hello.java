122bd91ab1f545e45a50b25d9523cfbf59e5f998
stelladream <neonykim@gmail.com>
2023-10-14 16:50:42 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -16,6 +16,6 @@ limitations under the License. */
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world from GIT!");
-        System.out.println("Changes after First commit!");
+        	System.out.println("Changes after First commit!");
 	}
 }
